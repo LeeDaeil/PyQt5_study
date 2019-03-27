219450eb34d17c14b69c29bde012d23c5989c8ae
DeailLee <dleodlf1004@naver.com>
2019-03-27 21:03:00 +0900

    0327_update

--- Study_8/study_8.py
+++
@@ -1,9 +0,0 @@
-import os
-
-file_list = os.listdir('./DB')
-with open('./DB/{}'.format(file_list[0])) as f:
-    temp = f.read()
-
-print(temp.count('제어봉'))
-
-
---
+++ ui_data/gui_study_8.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'study_8.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.setEnabled(True)
+        Dialog.resize(400, 300)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Dialog.sizePolicy().hasHeightForWidth())
+        Dialog.setSizePolicy(sizePolicy)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+
