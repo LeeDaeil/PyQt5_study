01374e67fdcabac6fccbc25b12361dd7b5beec3e
DeailLee <dleodlf1004@naver.com>
2019-03-29 15:51:38 +0900

    0329_update_make_gui

--- Study_9/Study_9.py
+++ Study_9/Study_9.py
@@ -1,14 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QDialog, QApplication
-from ui_data.gui_study_8 import *
-
+from ui_data.gui_study_9 import *
+import Study_9.Study_9_re_rc
 
 class MyForm(QDialog):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        self.show()
+        # self.show()
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
